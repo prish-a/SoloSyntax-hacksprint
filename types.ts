42aae0c989d4fa4d6e14db872a905da439b98887
Prisha B <147621864+prish-a@users.noreply.github.com>
2025-01-04 10:22:21 +0530

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,12 @@
+export interface InputFieldProps {
+  label: string;
+  type: string;
+  placeholder: string;
+  id: string;
+}
+
+export interface SocialSignInButtonProps {
+  icon: string;
+  provider: string;
+  alt: string;
+}
